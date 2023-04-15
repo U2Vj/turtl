2449e86315ee9445f14dff22e7008e9ec0e82cbb
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-04-15 16:37:58 +0200

    Add reactivity to CreateClassroomInstance

--- frontend2/src/views/CreateClassroomInstance.vue
+++ frontend2/src/views/CreateClassroomInstance.vue
@@ -1,178 +1,177 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import TurtlHeader from '@/components/TurtlHeader.vue'
+import { watch } from 'vue';
 
 const classroomName = ref('')
-const selected = ref()
-const selectedItemName = ref('')
-const search = ref(selectedItemName)
+const selected = ref<any[]>([])
 
-function rowSelected() {
-  if (selected.value.length === 1) {
-    selectedItemName.value = selected.value[0].name
-  } else {
-    selectedItemName.value = ''
-  }
-}
+const search = ref('')
 
 function createClassroom() {
-  //Selected Row Object in value: selected
+    // TODO: Button Classroom. Selected Row Object in value: selected
 }
 
 const headers = [
-  { title: 'Name', key: 'name' },
-  { title: 'Created On', key: 'created_on' },
-  { title: 'Created By', key: 'created_by' }
-]
+          { title: 'Name', key: 'name' },
+          { title: 'Created On', key: 'created_on' },
+          { title: 'Created By', key: 'created_by' },
+        ]
 const classTemplates = [
-  {
-    name: 'Test Networks',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Test Networks',
-    created_on: '01.03.2023',
-    created_by: 'Franz Mustermann'
-  },
-  {
-    name: 'Computer Versuch',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Safe',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Sicherheit',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Labor Networks',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Computer Programm',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Meister Teil',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Birnen Kompott',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Apfelmouse',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Advanced Software Engineering',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Data Science',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Digital Story',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Theo Inf',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Mathe',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Physik',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  },
-  {
-    name: 'Algorithmen',
-    created_on: '01.03.2023',
-    created_by: 'Max Mustermann'
-  }
+    {
+        name: 'Test Networks',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+        {
+        name: 'Test Networks',
+        created_on: '01.03.2023',
+        created_by: 'Franz Mustermann',
+    },
+    {
+        name: 'Computer Versuch',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Safe',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Sicherheit',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Labor Networks',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Computer Programm',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Meister Teil',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Birnen Kompott',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Apfelmouse',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Advanced Software Engineering',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Data Science',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Digital Story',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Theo Inf',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Mathe',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Physik',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
+    {
+        name: 'Algorithmen',
+        created_on: '01.03.2023',
+        created_by: 'Max Mustermann',
+    },
 ]
+
+watch(selected, (newSelected) => {
+    search.value = newSelected[0] ? newSelected[0].name : ''
+})
+
 </script>
 
 <template>
-  <turtl-header></turtl-header>
-  <v-main>
+    <turtl-header></turtl-header>
+    <v-main>
     <div class="classroomInstanceDiv">
-      <div class="inputMargins">
-        <h1 class="title">Create Classroom</h1>
-        <p class="namesTitle">Classroom Name</p>
-        <v-text-field
-          clearable
-          label="Type in Classroom Name"
-          v-model="classroomName"
-          variant="solo"
-          class="textFieldMargins"
-        ></v-text-field>
-        <p class="namesTitle">Classroom Template</p>
-        <v-text-field
-          clearable
-          label="Search Classroom Template"
-          v-model="selectedItemName"
-          append-icon="mdi-magnify"
-          variant="solo"
-          class="textFieldMargins"
-        ></v-text-field>
-      </div>
-      <div>
-        <v-data-table
-          @click="rowSelected()"
-          :headers="headers"
-          :items="classTemplates"
-          :search="search"
-          v-model="selected"
-          item-value="classTemplates"
-          show-select
-        ></v-data-table>
-      </div>
-      <v-btn @click="createClassroom">Create Classroom</v-btn>
-      <!-- <p>Selected2: {{ selected }}</p> -->
+        <div class="inputMargins">
+            <h1 class="title">Create Classroom</h1>
+            <p class="namesTitle">Classroom Name</p>
+            <v-text-field
+                clearable
+                label="Type in Classroom Name"
+                v-model="classroomName"
+                variant="solo"
+                class="textFieldMargins"
+            ></v-text-field>
+            <p class="namesTitle">Classroom Template</p>
+            <v-text-field
+                clearable
+                label="Search Classroom Template"
+                v-model="search"
+                append-icon="mdi-magnify"
+                variant="solo"
+                class="textFieldMargins"
+            ></v-text-field>
+            </div>
+            <div>
+            <v-data-table
+                :headers="headers"
+                :items="classTemplates"
+                :search="search"
+                v-model="selected"
+                item-value="classTemplates"
+                show-select
+            ></v-data-table>
+            </div>
+                <v-btn @click="createClassroom">Create Classroom</v-btn>
+                <!-- <p>Selected2: {{ selected }}</p> -->
+                <p>Selected:{{ selected }}</p>
     </div>
-  </v-main>
+    </v-main>
 </template>
 
 <style>
+
 .namesTitle {
-  font-weight: bold;
-  margin-bottom: 2%;
+    font-weight: bold;
+    margin-bottom: 2%;
 }
 .title {
-  font-weight: bolder;
-  margin-bottom: 10%;
+    font-weight: bolder;
+    margin-bottom: 10%;
 }
 .classroomInstanceDiv {
-  text-align: left;
-  margin-left: 5%;
-  margin-top: 2.5%;
-  margin-right: 20%;
+    text-align: left;
+    margin-left: 5%;
+    margin-top: 2.5%;
+    margin-right: 20%;
 }
 .inputMargins {
-  margin-right: 60%;
+    margin-right: 60%;
 }
 .textFieldMargins {
-  margin-bottom: 3%;
+    margin-bottom: 3%;
 }
 </style>
