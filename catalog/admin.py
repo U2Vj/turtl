52119637771e3bc9010a9021620410e5168f6012
Niklas <niklas.rubel@study.hs-duesseldorf.de>
2023-05-06 16:35:04 +0200

    Admin page register models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ClassroomTemplate, ProjectTemplate, ProjectBadge, TaskTemplate
+
 
 # Register your models here.
+admin.site.register(ClassroomTemplate)
+
+admin.site.register(ProjectBadge)
+
+admin.site.register(ProjectTemplate)
+
+admin.site.register(TaskTemplate)
+
