a9134a9a6114eb1aaed909656d31c3126f0d2905
Simon Ludwig <simon.ludwig@study.hs-duesseldorf.de>
2023-05-20 10:49:32 +0200

    fixup! added AutoPermissionViewSetWithListMixin

--- authentication/utils/permissions.py
+++ authentication/utils/permissions.py
@@ -3,10 +3,10 @@ from rules.contrib.rest_framework import AutoPermissionViewSetMixin
 
 class AutoPermissionViewSetWithListMixin(AutoPermissionViewSetMixin):
     """
-    This extends the default AutoPermissionViewSetMixin in Django Rules to also check read permissions when a request to
+    This extends the default AutoPermissionViewSetMixin in Django Rules to also check view permissions when a request to
     a list endpoint is being done.
     """
     permission_type_map = {
         **AutoPermissionViewSetMixin.permission_type_map,
-        "list": "read"
+        "list": "view"
     }
