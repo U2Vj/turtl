1e982f2d992c39a43ce6051fa216651f784aaab0
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-06-12 13:58:28 +0200

    Change card in TemplateCard

--- frontend/src/components/TemplateCard.vue
+++ frontend/src/components/TemplateCard.vue
@@ -24,10 +24,10 @@ useSortable(`#taskWrapper${props.projectId}`, props.tasks, {
 
 <template>
   <v-card max-width="1000" variant="flat" color="cardColor" class="elevation-4" style="cursor: grab">
-    <v-container fluid>
-      <v-row>
-        <v-col md="6">
-          <div><v-icon icon="mdi-drag" />{{ name }}</div>
+      <v-card-title>
+              <v-icon icon="mdi-drag" />{{ name }}
+      </v-card-title>
+      <v-card-actions>
           <v-btn
             v-if="!showInformation"
             append-icon="mdi-chevron-down"
@@ -44,24 +44,20 @@ useSortable(`#taskWrapper${props.projectId}`, props.tasks, {
           >
             Hide Information
           </v-btn>
-        </v-col>
-      </v-row>
-      <v-row v-show="showInformation">
-        <v-col>
-          <div>Tasks</div>
+          </v-card-actions>
+      <v-card-text v-show="showInformation">
+          Tasks
           <div :id="`taskWrapper${props.projectId}`">
             <div v-for="task in props.tasks" :key="task.id" style="cursor: grab">
               <v-icon icon="mdi-drag" />{{ task.name }}
             </div>
           </div>
+      <v-card-actions>
           <v-btn prepend-icon="mdi-plus" variant="tonal" color="primary" class="elevation-2">Add Task</v-btn>
-        </v-col>
-        <v-col md="6">
-          <div class="ml-auto" style="width: fit-content">
-            <v-btn variant="tonal" color="error" class="elevation-2">Delete</v-btn>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
+          <v-spacer></v-spacer>
+            <v-btn variant="tonal" color="error" class="elevation-2">Delete Template</v-btn>
+      </v-card-actions>
+      </v-card-text>
+
   </v-card>
 </template>
