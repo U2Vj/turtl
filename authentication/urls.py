cde21d278a7796800e785edcd4bedc3724b61b27
Niklas <niklas.rubel@study.hs-duesseldorf.de>
2023-06-24 19:48:24 +0200

    Changed urls paths

--- authentication/urls.py
+++ authentication/urls.py
@@ -12,10 +12,4 @@ urlpatterns = [
     path('users/register', RegistrationAPIView.as_view()),
     path('users/login', LoginAPIView.as_view()),
     path('user', UserRetrieveUpdateAPIView.as_view()),
-    path('/templates/classrooms', ClassroomTemplateList.as_view(), name='classroom-template-list'),
-    path('/templates/classrooms/<int:template_id>', ClassroomTemplateDetail.as_view(), name='classroom-template-detail'),
-    path('projects/', ProjectTemplateList.as_view(), name='project-template-list'),
-    path('projects/<int:project_id>/', ProjectTemplateDetail.as_view(), name='project-template-detail'),
-    path('tasks/', TaskTemplateList.as_view(), name='task-template-list'),
-    path('tasks/<int:task_id>/', TaskTemplateDetail.as_view(), name='task-template-detail')
 ]
