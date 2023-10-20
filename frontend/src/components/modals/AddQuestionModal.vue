61e90e1dd7eddf05d50b893c3a41004a3d1e9fc9
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-10-20 17:27:49 +0200

    Implemented Add Questionnaire Modal

--- frontend/src/components/modals/AddQuestionModal.vue
+++ frontend/src/components/modals/AddQuestionModal.vue
@@ -2,13 +2,22 @@
 import PrimaryButton from '@/components/buttons/PrimaryButton.vue'
 import TextButton from '@/components/buttons/TextButton.vue'
 import { ref } from 'vue'
+import ErrorButton from '../buttons/ErrorButton.vue'
+import SecondaryButton from '../buttons/SecondaryButton.vue'
 
 const showDialog = ref(false)
 const newQuestionnaire = ref('')
 
 const singleChoice = ref(false)
-const answer1 = ref(false)
-const labelAnswer1 = ref('')
+
+const answerOptions = ref([{ answer: '', checked: false }])
+
+const addAnswerOption = () => {
+  answerOptions.value.push({ answer: '', checked: false })
+}
+const removeAnswerOption = (index: number) => {
+  answerOptions.value.splice(index, 1)
+}
 </script>
 
 <template>
@@ -25,23 +34,49 @@ const labelAnswer1 = ref('')
           label="New Questionnaire"
         ></v-text-field>
         <v-checkbox v-model="singleChoice" label="Single Choice"></v-checkbox>
-        <div style="display: flex; justify-content: start">
-          <div>
-            <v-checkbox v-model="answer1"></v-checkbox>
-          </div>
-          <div style="flex: 1">
-            <v-text-field
-              clearable
-              variant="underlined"
-              base-color="primary"
-              color="primary"
-              v-model="labelAnswer1"
-              label="Answer"
-            ></v-text-field>
-          </div>
-        </div>
-        <TextButton buttonName="Close" @click="showDialog = false"></TextButton>
-        <PrimaryButton buttonName="Create"> </PrimaryButton>
+        <v-row>
+          <v-col>
+            <div
+              v-for="(option, index) in answerOptions"
+              :key="index"
+              style="display: flex; justify-content: start"
+            >
+              <div>
+                <v-checkbox v-model="option.checked"></v-checkbox>
+              </div>
+              <div style="flex-grow: 1">
+                <v-text-field
+                  clearable
+                  variant="underlined"
+                  base-color="primary"
+                  color="primary"
+                  v-model="option.answer"
+                  label="Answer"
+                ></v-text-field>
+                <div>
+                  <ErrorButton
+                    button-name="Delete"
+                    @click="removeAnswerOption(index)"
+                  ></ErrorButton>
+                </div>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <SecondaryButton
+              button-name="Add Answer Option"
+              @click="addAnswerOption"
+            ></SecondaryButton>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <TextButton buttonName="Close" @click="showDialog = false"></TextButton>
+            <PrimaryButton buttonName="Create"> </PrimaryButton>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-dialog>
