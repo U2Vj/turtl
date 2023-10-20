82dc6cdd35ce21a7db51ce263ce153a00a53cf17
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-10-20 16:29:20 +0200

    delete unnecessary code

--- frontend/src/views/instructor/InstructorTask.vue
+++ frontend/src/views/instructor/InstructorTask.vue
@@ -111,54 +111,6 @@ try {
             <h3>Questionnaire</h3>
           </v-col>
         </v-row>
-        <v-row v-if="task.acceptance_criteria.acceptance_criteria_questionnaire">
-          <v-col
-            cols="12"
-            v-for="question in task.acceptance_criteria.acceptance_criteria_questionnaire"
-            :key="question.id"
-          >
-            <v-form>
-              <v-card variant="flat" color="cardColor" class="elevation-4">
-                <v-card-text>
-                  <v-text-field
-                    label="Edit Question"
-                    clearable
-                    variant="underlined"
-                    base-color="primary"
-                    color="primary"
-                    v-model="question.title"
-                  ></v-text-field>
-                  <v-row v-for="choice in question.choices" :key="choice.answer">
-                    <v-col>
-                      <v-text-field
-                        label="Edit Answer"
-                        clearable
-                        variant="underlined"
-                        base-color="primary"
-                        color="primary"
-                        v-model="choice.answer"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="3">
-                      <!-- TODO: Make this a checkbox -->
-                      <v-select
-                        label="Correct"
-                        :items="[
-                          { title: 'True', value: true },
-                          { title: 'False', value: false }
-                        ]"
-                        variant="underlined"
-                        base-color="primary"
-                        color="primary"
-                        v-model="choice.is_correct"
-                      ></v-select>
-                    </v-col>
-                  </v-row>
-                </v-card-text>
-              </v-card>
-            </v-form>
-          </v-col>
-        </v-row>
         <v-row>
           <v-col>
             <SecondaryButton buttonName="Add Questionnaire">
@@ -181,42 +133,40 @@ try {
         </v-row>
         <v-row>
           <v-col>
-            <v-form>
-              <!-- TODO: make this a loop because a task can contain multiple virtualizations -->
-              <v-card
-                v-for="virtualization in task.virtualizations"
-                :key="virtualization.id"
-                variant="flat"
-                color="cardColor"
-                class="elevation-4"
-              >
-                <v-card-text>
-                  <v-text-field
-                    label="Name of Virtualization"
-                    clearable
-                    variant="underlined"
-                    base-color="primary"
-                    color="primary"
-                    v-model="virtualization.name"
-                  ></v-text-field>
-                  <v-select
-                    label="Role"
-                    :items="['UserShell', 'IP']"
-                    variant="underlined"
-                    base-color="primary"
-                    color="primary"
-                    v-model="virtualization.role"
-                  ></v-select>
-                  <v-file-input
-                    clearable
-                    label="Dockerfile"
-                    variant="underlined"
-                    base-color="primary"
-                    color="primary"
-                  ></v-file-input>
-                </v-card-text>
-              </v-card>
-            </v-form>
+            <!-- TODO: make this a loop because a task can contain multiple virtualizations -->
+            <v-card
+              v-for="virtualization in task.virtualizations"
+              :key="virtualization.id"
+              variant="flat"
+              color="cardColor"
+              class="elevation-4"
+            >
+              <v-card-text>
+                <v-text-field
+                  label="Name of Virtualization"
+                  clearable
+                  variant="underlined"
+                  base-color="primary"
+                  color="primary"
+                  v-model="virtualization.name"
+                ></v-text-field>
+                <v-select
+                  label="Role"
+                  :items="['UserShell', 'IP']"
+                  variant="underlined"
+                  base-color="primary"
+                  color="primary"
+                  v-model="virtualization.role"
+                ></v-select>
+                <v-file-input
+                  clearable
+                  label="Dockerfile"
+                  variant="underlined"
+                  base-color="primary"
+                  color="primary"
+                ></v-file-input>
+              </v-card-text>
+            </v-card>
           </v-col>
         </v-row>
         <div class="d-flex mt-5 mb-2 align-center justify-space-between">
