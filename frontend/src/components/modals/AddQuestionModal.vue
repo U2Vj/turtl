6e406adac137028223d855381a07bd785b06288c
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-10-20 15:52:54 +0200

    Start Adding Questinonnaire

--- frontend/src/components/modals/AddQuestionModal.vue
+++ frontend/src/components/modals/AddQuestionModal.vue
@@ -1,44 +1,30 @@
 <script setup lang="ts">
-import { makeAPIRequest } from '@/communication/APIRequests'
 import PrimaryButton from '@/components/buttons/PrimaryButton.vue'
 import TextButton from '@/components/buttons/TextButton.vue'
-import { useCatalogStore } from '@/stores/CatalogStore'
 import { ref } from 'vue'
 
 const showDialog = ref(false)
-const newQuestion = ref('')
+const newQuestionnaire = ref('')
 
-async function addQuestion(title: string) {
-  // TODO: make this actually work
-  const catalogStore = useCatalogStore()
-  const classroomId = catalogStore.classroom?.id
-  const data = {
-    title,
-    classroomId
-  }
-
-  const response = await makeAPIRequest('/templates/classrooms', 'POST', true, true, data)
-  if (response) {
-    showDialog.value = false
-  }
-}
+const singleChoice = ref(false)
 </script>
 
 <template>
   <v-dialog v-model="showDialog" activator="parent" persistent width="50%">
     <v-card>
-      <v-card-title>Add Question</v-card-title>
+      <v-card-title>Add Questionnaire</v-card-title>
       <v-card-text>
         <v-text-field
           clearable
           variant="underlined"
           base-color="primary"
           color="primary"
-          v-model="newQuestion"
-          label="New Question"
+          v-model="newQuestionnaire"
+          label="New Questionnaire"
         ></v-text-field>
+        <v-checkbox v-model="singleChoice" label="Single Choice"></v-checkbox>
         <TextButton buttonName="Close" @click="showDialog = false"></TextButton>
-        <PrimaryButton buttonName="Create" @click="addQuestion(newQuestion)"> </PrimaryButton>
+        <PrimaryButton buttonName="Create"> </PrimaryButton>
       </v-card-text>
     </v-card>
   </v-dialog>
