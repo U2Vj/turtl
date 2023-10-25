de1725e950434c3811c5acf14572510c5678278b
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-10-25 15:27:44 +0200

    Change Project title work, but without success message and without error message

--- frontend/src/components/ProjectCard.vue
+++ frontend/src/components/ProjectCard.vue
@@ -5,7 +5,14 @@ import TextButton from '@/components/buttons/TextButton.vue'
 import AddTaskModal from '@/components/modals/AddTaskModal.vue'
 import DeleteProjectModal from '@/components/modals/DeleteProjectModal.vue'
 import type { Task } from '@/stores/CatalogStore'
-import { ref } from 'vue'
+import { useCatalogStore } from '@/stores/CatalogStore'
+import { ref, toRef } from 'vue'
+import { useToast } from 'vue-toastification'
+import PrimaryButton from './buttons/PrimaryButton.vue'
+
+const catalogStore = useCatalogStore()
+const toast = useToast()
+let classroom = toRef(catalogStore, 'classroom')
 
 const emit = defineEmits<{
   (e: 'update:task', id: number, event: any): void
@@ -19,6 +26,27 @@ const props = defineProps<{
 }>()
 
 const showDetails = ref(false)
+const projectTitle = ref(props.projectTitle)
+
+function editProjectTitle(newTitle: string, projectId: number) {
+  if (!classroom.value) {
+    return
+  }
+
+  const updatedClassroom = { ...classroom.value }
+
+  const project = updatedClassroom.projects.find((p) => p.id === projectId)
+
+  if (project) {
+    project.title = newTitle
+
+    catalogStore
+      .updateClassroom(classroom.value.id, updatedClassroom)
+      .catch((e) => toast.error(e.message))
+  } else {
+    console.error('Projekt nicht gefunden')
+  }
+}
 </script>
 
 <template>
@@ -39,6 +67,19 @@ const showDetails = ref(false)
       ></TextButton>
     </v-card-actions>
     <v-card-text v-show="showDetails">
+      <v-text-field
+        label="Edit Title"
+        clearable
+        variant="underlined"
+        base-color="primary"
+        color="primary"
+        v-model="projectTitle"
+      ></v-text-field>
+      <PrimaryButton
+        button-name="Save Title"
+        @click="editProjectTitle(projectTitle, projectId)"
+      ></PrimaryButton>
+      <br />
       Tasks
       <div>
         <div v-for="task in props.tasks" :key="task.id">
