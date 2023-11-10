6bc9458caaebb0bebcbaffc342da4d9e58de9948
Simon Ludwig <info@simonludwig.com>
2023-11-10 15:13:30 +0100

    added classroom details to enrollment endpoint, added permissions and refactored code

--- enrollments/views.py
+++ enrollments/views.py
@@ -1,53 +1,27 @@
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework import status
-from rest_framework.exceptions import NotFound, PermissionDenied, ValidationError
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rules.contrib.rest_framework import AutoPermissionViewSetMixin
 
-from catalog.models import Classroom
-from turtl.utils.permissions import AutoPermissionViewSetWithListMixin
 from .models import Enrollment
-from .serializers import EnrollmentSerializer
+from .serializers import EnrollmentSerializer, EnrollmentDetailSerializer
 
 
-class EnrollmentViewSet(AutoPermissionViewSetWithListMixin, ModelViewSet):
+class EnrollmentViewSet(ModelViewSet):
     permission_classes = (IsAuthenticated,)  # A user must be authenticated to access their enrollments
     serializer_class = EnrollmentSerializer
 
     def get_queryset(self):
         return Enrollment.objects.filter(student=self.request.user)
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        return context
 
     def perform_create(self, serializer):
-        classroom_id = self.request.data.get('classroom')
-        student = self.request.user
+        serializer.save(student=self.request.user)
 
-        try:
-            Classroom.objects.get(id=classroom_id)
-        except ObjectDoesNotExist:
-            raise ValidationError('The classroom you\'re trying to enroll in does not exist.')
 
-        if Enrollment.objects.filter(classroom=classroom_id, student=student).exists():
-            raise ValidationError('You are already enrolled in this classroom.')
-
-        serializer.save(student=student)
-
-    def destroy(self, request, *args, **kwargs):
-        try:
-            instance = self.get_object()
-        except Enrollment.DoesNotExist:
-            raise NotFound("Enrollment not found.")
-
-        if instance.student != self.request.user and not self.request.user.is_administrator:
-            raise PermissionDenied('You do not have permission to delete this enrollment.')
-
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def perform_destroy(self, instance):
-        instance.delete()
+class EnrollmentDetailViewSet(AutoPermissionViewSetMixin, ModelViewSet):
+    serializer_class = EnrollmentDetailSerializer
+    queryset = Enrollment.objects.all()
