d5287601508247fd5f49e5affd0023760bd0cc45
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-11-18 19:03:52 +0100

    Add Sidebar when small screen

--- frontend/src/components/HeaderTurtl.vue
+++ frontend/src/components/HeaderTurtl.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/UserStore'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useToast } from 'vue-toastification'
 
@@ -11,11 +12,14 @@ function handleLogout() {
   userStore.logout(router)
   toast.info('You have been signed out.')
 }
+
+const drawer = ref(false)
 </script>
 
 <template>
   <div>
     <v-app-bar sticky color="cardColor" class="elevation-4">
+      <v-app-bar-nav-icon @click="drawer = true" class="d-flex d-sm-none"></v-app-bar-nav-icon>
       <div v-if="userStore.isAdministrator()" class="flex-grow-1">
         <v-btn to="/instructor/classrooms/my">My Classrooms</v-btn>
         <v-btn to="/student/enrollments">My Enrollments</v-btn>
@@ -61,5 +65,41 @@ function handleLogout() {
         </v-menu>
       </template>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-if="userStore.isAdministrator()">
+          <v-list-item to="/instructor/classrooms/my">
+            <v-list-item-title>My Classrooms</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/student/enrollments">
+            <v-list-item-title>My Enrollments</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/instructor/invitations">
+            <v-list-item-title>Invite Users</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+
+        <v-list-item-group v-if="userStore.isInstructor()">
+          <v-list-item to="/instructor/classrooms/my">
+            <v-list-item-title>My Classrooms</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/student/enrollments">
+            <v-list-item-title>My Enrollments</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/instructor/invitations">
+            <v-list-item-title>Invite Users</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+
+        <v-list-item-group v-if="userStore.isStudent()">
+          <v-list-item to="/student/enrollments">
+            <v-list-item-title>My Enrollments</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="/student/classrooms/all">
+            <v-list-item-title>All Classrooms</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
