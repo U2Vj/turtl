cb0bf17d54a0fe24cd465280b57b8660952acfda
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-12-03 17:45:19 +0100

    Modal when try to delete yourself out of the classroom

--- frontend/src/components/modals/AddInstructorModal.vue
+++ frontend/src/components/modals/AddInstructorModal.vue
@@ -1,30 +1,43 @@
 <script setup lang="ts">
+import { makeAPIRequest } from '@/communication/APIRequests'
 import TextButton from '@/components/buttons/TextButton.vue'
-import {ref, toRef} from 'vue'
-import { makeAPIRequest } from "@/communication/APIRequests";
-import {useToast} from "vue-toastification";
-import type {User} from "@/stores/UserStore";
-import {useCatalogStore} from "@/stores/CatalogStore";
+import { useCatalogStore } from '@/stores/CatalogStore'
+import type { User } from '@/stores/UserStore'
+import { useUserStore } from '@/stores/UserStore'
+import { ref, toRef } from 'vue'
+import { useToast } from 'vue-toastification'
+import PrimaryButton from '../buttons/PrimaryButton.vue'
+
+const userStore = useUserStore()
 
 const toast = useToast()
 const instructors = ref<User[]>()
 
 const catalogStore = useCatalogStore()
 const classroom = toRef(catalogStore, 'classroom')
+const currentInstructorId = ref()
+const showDialogInstructor = ref(false)
 
-makeAPIRequest("/users/instructors", 'GET', true, true).then((response) => {
-  instructors.value = response.data
-}).catch((e) => {
-  toast.error(e.message)
-})
+makeAPIRequest('/users/instructors', 'GET', true, true)
+  .then((response) => {
+    instructors.value = response.data
+  })
+  .catch((e) => {
+    toast.error(e.message)
+  })
 
 const showDialog = ref(false)
 
 function instructorInClassroom(id: number): boolean {
-  if(!classroom.value) {
+  if (!classroom.value) {
     return false
   }
-  return classroom.value.instructors.filter(item => item.instructor.id === id).length > 0
+  return classroom.value.instructors.filter((item) => item.instructor.id === id).length > 0
+}
+
+function confirmInstructorRemoval(instructorId: number) {
+  currentInstructorId.value = instructorId
+  showDialogInstructor.value = true
 }
 </script>
 
@@ -47,9 +60,14 @@ function instructorInClassroom(id: number): boolean {
               color="error"
               @click="
                 () => {
-                  catalogStore.removeInstructor(item.raw.id)
-                    .then(() => toast.info('Instructor removed'))
-                    .catch((e) => toast.error(e.message))
+                  if (item.raw.id === userStore.user?.id) {
+                    confirmInstructorRemoval(item.raw.id)
+                  } else {
+                    catalogStore
+                      .removeInstructor(item.raw.id)
+                      .then(() => toast.info('Instructor removed'))
+                      .catch((e) => toast.error(e.message))
+                  }
                 }
               "
               v-if="instructorInClassroom(item.raw.id)"
@@ -59,13 +77,40 @@ function instructorInClassroom(id: number): boolean {
               variant="text"
               @click="
                 () => {
-                  catalogStore.addInstructor(item.raw.id)
+                  catalogStore
+                    .addInstructor(item.raw.id)
                     .then(() => toast.success('Instructor added'))
                     .catch((e) => toast.error(e.message))
                 }
               "
               v-else
             />
+            <v-dialog v-model="showDialogInstructor">
+              <v-card>
+                <v-card-title>Delete Task</v-card-title>
+                <v-card-text>
+                  <p>Are you sure you want to remove yourself from this classroom?</p>
+                </v-card-text>
+                <v-card-actions>
+                  <TextButton buttonName="Close" @click="showDialogInstructor = false"></TextButton>
+                  <PrimaryButton
+                    buttonName="Delete"
+                    @click="
+                      () => {
+                        catalogStore
+                          .removeInstructor(currentInstructorId)
+                          .then(() => toast.info('Instructor removed'))
+                          .catch((e) => toast.error(e.message))
+                        showDialog = false
+                        $router.push({
+                          name: 'InstructorMyClassrooms'
+                        })
+                      }
+                    "
+                  ></PrimaryButton>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </template>
         </v-data-table>
         <TextButton buttonName="Close" @click="showDialog = false"></TextButton>
