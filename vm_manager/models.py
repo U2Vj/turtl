220db167d0d68a1c2f0033d215d06b27fc9c160a
alada002 <alexander.adamski002@stud.fh-dortmund.d>
2025-07-11 12:54:43 +0000

    add existing models to vm_manager

--- vm_manager/models.py
+++ vm_manager/models.py
@@ -1,3 +1,169 @@
 from django.db import models
+from django.conf import settings
+from catalog.models import Task
+from django.core.validators import validate_ipv4_address
 
 # Create your models here.
+
+# Network Instance
+class Network(models.Model):
+    name = models.CharField(max_length=255)
+    subnet = models.CharField(max_length=20)
+    vlan_id = models.IntegerField(unique=True, null=True, blank=True)
+    template = models.ForeignKey('NetworkTemplate', on_delete=models.SET_NULL, null=True, related_name="networks")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='networks')
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='networks')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.subnet}) for {self.user} in {self.task}"
+
+# -----------------------
+# TEMPLATES
+# -----------------------
+
+class VMTemplate(models.Model):
+    """
+    For VM templates in proxmox that can be cloned for tasks
+    """
+    name = models.CharField(max_length=255)
+    template_id = models.IntegerField(unique=True)
+    description = models.TextField(blank=True)
+    #Resources
+    cpu_cores = models.IntegerField(default=1)
+    memory_mb = models.IntegerField(default=512)
+    disk_gb = models.IntegerField(default=10)
+    #Purpose
+    PURPOSE_CHOICES = [
+        ('USER_SHELL', 'User Shell'),
+        ('TASK_SUPPORT', 'Available inside a task as support')
+    ]
+    purpose = models.CharField(max_length=20, choices=PURPOSE_CHOICES)
+
+    def __str__(self):
+        return f"{self.name} (ID: {self.template_id})"
+
+class NetworkTemplate(models.Model):
+    """
+    Network configuration template for interconnected VMs
+    """
+    name = models.CharField(max_length=255)
+    subnet = models.CharField(max_length=20)
+    vlan_id = models.IntegerField(null=True, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.subnet})"
+    
+# -----------------------
+# BLUEPRINTS
+# -----------------------
+
+class TaskVMConfiguration(models.Model):
+    """
+    Links task to VM templates and network config
+    Used as a Blueprint for later user environments
+    """
+    task = models.OneToOneField(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='vm_configuration'
+    )
+    network_template = models.ForeignKey(
+        NetworkTemplate,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="task_configurations"
+    )
+
+    allow_internet_access = models.BooleanField(default=False)
+    max_runtime_hours = models.IntegerField(default=12)
+    auto_cleanup_after_hours = models.IntegerField(default=48)
+
+    def __str__(self):
+        return f"VM Configuration for {self.task.title}"
+
+class TaskVMTemplate(models.Model):
+    """
+    Contains VM template needed for a task with configuration
+    """
+
+    configuration = models.ForeignKey(
+        TaskVMConfiguration,
+        on_delete=models.CASCADE,
+        related_name='vm_templates'
+    )
+    template = models.ForeignKey(
+        VMTemplate,
+        on_delete=models.PROTECT
+    )
+
+    planned_ip_address = models.CharField(max_length=15, validators=[validate_ipv4_address], null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.template.name} for {self.configuration.task.title}"
+
+# User instance of task    
+class LabEnvironment(models.Model):
+    """
+    Groups all parts of the provisioned environment for an individual user and task
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="lab_environments")
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="lab_environments")
+    network = models.ForeignKey(Network, on_delete=models.CASCADE, related_name="lab_environments")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    STATUS_CHOICES = [
+        ('provisioning', 'Provisioning'),
+        ('active', 'Active'),
+        ('suspended', 'Suspended'),
+        ('cleanup', 'Cleanup'),
+    ]
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='provisioning')
+
+    class Meta:
+        unique_together = ("user", "task")
+    
+    def __str__(self):
+        return f"LabEnvironment for {self.user} in {self.task}"
+
+# Instance of a VM inside a task
+class VirtualMachine(models.Model):
+    """
+    Represents an VM instance for a specific user and task
+    """
+    STATUS_CHOICES = [
+        ('running', 'Running'),
+        ('stopped', 'Stopped'),
+        ('paused', 'Paused'),
+        ('suspended', 'Suspended'),
+        ('creating', 'Creating'),
+        ('error', 'Error'),
+    ]
+    #The ID of the VM in Proxmox
+    vmid = models.IntegerField(unique=True)
+    # Environment the VM belongs to
+    lab_environment = models.ForeignKey(
+        LabEnvironment,
+        on_delete=models.CASCADE,
+        related_name="virtual_machines"
+    )
+    #template used to create the VM
+    template = models.ForeignKey(VMTemplate, on_delete=models.SET_NULL, null=True)
+    #VM Name in Proxmox
+    name = models.CharField(max_length=255)
+    #Current status of the VM
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='creating')
+    status_changed_at = models.DateTimeField(auto_now_add=True)
+    error_message = models.TextField(blank=True, null=True)
+    #Network config
+    network = models.ForeignKey('Network', on_delete=models.SET_NULL, null=True, related_name='vms')
+    assigned_ip_address = models.GenericIPAddressField(null=True, blank=True)
+    #Dates
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #class Meta:
+        #unique_together = (('lab_environment', 'template'),)
