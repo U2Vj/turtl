00a273789527b2674b321fed6aba1d16117a1588
alada002 <alexander.adamski002@stud.fh-dortmund.d>
2026-04-19 22:56:30 +0000

    fix asgi

--- turtl/asgi.py
+++ turtl/asgi.py
@@ -1,14 +1,14 @@
 # mysite/asgi.py
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turtl.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import shell.routing
 from shell.auth import JwtAuthMiddlewareStack
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turtl.settings")
 
 
 
