d4bd2f9ae019810b3f6792a7bb1b3e13c3e415e4
alada002 <alexander.adamski002@stud.fh-dortmund.d>
2026-04-19 23:04:49 +0000

    fix asgi 2

--- turtl/asgi.py
+++ turtl/asgi.py
@@ -3,17 +3,17 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "turtl.settings")
 
-from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 import shell.routing
 from shell.auth import JwtAuthMiddlewareStack
 
 
-
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": django_asgi_app,
   "websocket": AllowedHostsOriginValidator(
         JwtAuthMiddlewareStack(
             URLRouter(
