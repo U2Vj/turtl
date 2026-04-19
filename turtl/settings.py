765987a2f019d965cd2be2445442eb3b704e705a
alada002 <alexander.adamski002@stud.fh-dortmund.d>
2026-04-19 16:51:00 +0000

    adjusted nginx conf for ip deployment

--- turtl/settings.py
+++ turtl/settings.py
@@ -181,15 +181,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Security (for HTTPS reverse-proxy deployment)
+# Production ssl settings
 if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
 
 
 
