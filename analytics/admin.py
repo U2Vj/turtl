4d2bcaef42d38d899330e07989328bd84b4facc9
alada002 <alexander.adamski002@stud.fh-dortmund.d>
2026-05-19 00:18:56 +0000

    add analytics event admin view

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import AnalyticsEvent
 
-# Register your models here.
+
+@admin.register(AnalyticsEvent)
+class AnalyticsEventAdmin(admin.ModelAdmin):
+    list_display = ['created_at', 'event_type', 'user', 'task', 'classroom', 'lab_environment_id']
+    list_filter = ['event_type', 'created_at']
