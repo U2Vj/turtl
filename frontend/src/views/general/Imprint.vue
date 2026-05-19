59aff1990e6a04b78b86e579fae489c1f07a5704
alada002 <alexander.adamski002@stud.fh-dortmund.d>
2026-05-19 00:31:10 +0000

    update imprint

--- frontend/src/views/general/Imprint.vue
+++ frontend/src/views/general/Imprint.vue
@@ -8,19 +8,18 @@ import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
     <template #default>
       <p>
         Project Title: TURTL<br />
-        Institution: Hochschule Düsseldorf - University of Applied Sciences<br />
-        Department: Medieninformatik<br />
-        Project Supervisors: Prof. Dr. Holger Schmidt, Prof. Dr.-Ing. M.Sc. Markus Dahm<br />
+        Institution: Fachhochschule Dortmund - University of Applied Sciences and Arts<br />
+        Department: Informatik<br />
+        Project Supervisors: Prof. Dr. Holger Schmidt<br />
         <br />
         Contact Information:<br />
-        Hochschule Düsseldorf<br />
-        University of Applied Sciences<br />
-        Münsterstraße 156<br />
-        40476 Düsseldorf, Germany<br />
+        Fachhochschule Dortmund<br />
+        Sonnenstraße 96-100<br />
+        44139 Dortmund, Germany<br />
       </p>
       <br />
       <p>
-        Legal Form: The Hochschule Düsseldorf is a public corporation.<br />
+        Legal Form: The Fachhochschule Dortmund is a public corporation.<br />
         Regulatory Authority: Ministry of Culture and Science of the State of North Rhine-Westphalia
         (MKW NRW)<br />
         VAT Identification Number: DE [Number]<br />
@@ -28,14 +27,14 @@ import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
       <br />
       <p>
         Disclaimer: The content of this project has been carefully prepared and reviewed. However,
-        the Hochschule Düsseldorf does not guarantee the accuracy, completeness, or quality of the
+        the Fachhochschule Dortmund does not guarantee the accuracy, completeness, or quality of the
         information provided.<br />
       </p>
       <br />
       <p>
-        Copyright: © 2023, Hochschule Düsseldorf. All rights reserved. All content within the
+        Copyright: © 2026, Fachhochschule Dortmund. All rights reserved. All content within the
         project, including texts, graphics, and images, is protected by copyright and may not be
-        reproduced without the express permission of the Hochschule Düsseldorf.<br />
+        reproduced without the express permission of the Fachhochschule Dortmund.<br />
       </p>
     </template>
   </DefaultLayout>
